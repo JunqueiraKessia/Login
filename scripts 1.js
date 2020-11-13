3e9120b2984f3ee8dfba15a8e4005aeadcdf79c2
JunqueiraKessia <keujunqueira@gmail.com>
2020-11-13 14:18:36 -0300

    Commit inicial do logingit add .

---
+++ scripts 1.js
@@ -0,0 +1,12 @@
+function enviar() {
+
+  var user = document.getElementById('nome');
+  var senha= document.getElementById('senha');
+
+
+
+if (user.value=='Tiago Vieira' && senha.value=='12345') {
+  alert("Login realizado com sucesso!");
+
+}
+}
